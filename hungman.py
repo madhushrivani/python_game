710a448f5e9734773c8a2efdaec19d06d24ab24f
Madhu Shrivani <125638924+madhushrivani@users.noreply.github.com>
2026-06-20 21:28:54 +0530

    Add files via upload

---
+++ hungman.py
@@ -0,0 +1,77 @@
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = r''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+word_list=["madhu","tanya","surbhi"]
+random_word= random.choice(word_list)
+word_len= len(random_word)
+user_letter=input("enter the guess letter word")
+correct=[]
+for stages in range(word_len):
+    if user_letter==random_word:
+        correct+=user_letter
+    else:
+        logo
